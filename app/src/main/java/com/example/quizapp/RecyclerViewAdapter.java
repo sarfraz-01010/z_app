bce1269e82305ff6181adc0505d729e82a3a2164
sarfraz-01010 <sarfraz01010@gmail.com>
2023-07-13 00:17:55 +0500

    RecyclerViewAdaptar

--- app/src/main/java/com/example/quizapp/RecyclerViewAdapter.java
+++ app/src/main/java/com/example/quizapp/RecyclerViewAdapter.java
@@ -26,5 +26,35 @@ public class RecyclerViewAdaptar extends RecyclerView.Adapter<RecyclerViewAdapta
     public RecyclerViewAdaptar.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
         View itemView = LayoutInflater.from(parent.getContext())
                 .inflate(R.layout.single_result_item, parent, false);
-        return new ViewHolder(itemView);
+        return new RecyclerView.ViewHolder(itemView);
     }
+
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+//        Questions quizResult = quizResults.get(position);
+//        holder.bind(quizResult);
+
+        holder.data=quizResults.get(position);
+        holder.question.setText(holder.data.getQuestion());
+        holder.cAns.setText(String.valueOf(holder.data.getCorrectAnswer()));
+        holder.yAns.setText(holder.data.getUserAnswer());
+    }
+
+    @Override
+    public int getItemCount() {
+        return quizResults.size();
+    }
+
+    public static class ViewHolder extends RecyclerView.ViewHolder {
+
+        private final TextView question, cAns, yAns;
+        Questions data;
+
+        public ViewHolder(@NonNull View itemView) {
+            super(itemView);
+            question = itemView.findViewById(R.id.question);
+            cAns = itemView.findViewById(R.id.correctans);
+            yAns = itemView.findViewById(R.id.yourans);
+        }
+    }
+}
