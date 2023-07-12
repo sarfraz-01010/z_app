c3d5aacf6a8d409ce285a4ec54b61fc63a4c03e3
sarfraz-01010 <sarfraz01010@gmail.com>
2023-07-12 23:51:40 +0500

    Result adjustments

---
+++ app/src/main/java/com/example/quizapp/QuizResult.java
@@ -0,0 +1,24 @@
+package com.example.quizapp;
+
+
+
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Toast;
+
+import java.util.List;
+
+public class QuizResult extends Fragment {
+    DbHandler dbHandler;
+    public QuizResult() {
+        // Required empty public constructor
+    }
