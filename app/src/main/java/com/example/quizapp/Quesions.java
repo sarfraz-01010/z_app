d7fbf9e096fa1936cde63282cb3ab3f5c92d7fe8
sarfraz-01010 <sarfraz01010@gmail.com>
2023-07-12 23:38:32 +0500

    getters and setters

--- app/src/main/java/com/example/quizapp/Quesions.java
+++ app/src/main/java/com/example/quizapp/Quesions.java
@@ -8,15 +8,48 @@ public class Quesions {
 
         public String user_answer;
 
-        public Questions(){
+        public Questions() {
 
         }
 
-        public Questions(int id, String q, String a1, String a2){
+        public Questions(int id, String q, String a1, String a2) {
             this.id = id;
             question = q;
             correct_answer = a1;
             user_answer = a2;
         }
 
+        public void setId(int id) {
+            this.id = id;
+        }
+
+        public int getId() {
+            return this.id;
+        }
+
+        public void setQuestion(String question) {
+            this.question = question;
+        }
+
+        public String getQuestion() {
+            return this.question;
+        }
+
+        public void setCorrectAnswer(String answer) {
+            this.correct_answer = answer;
+        }
+
+        public String getCorrectAnswer() {
+            return this.correct_answer;
+        }
+
+        public void setUserAnswer(String answer) {
+            this.user_answer = answer;
+        }
+
+        public String getUserAnswer() {
+            return this.user_answer;
+        }
+
+    }
     }
