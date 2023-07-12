8efe81b9a6d1540bfaf3316b9a8bf03423ddb4b0
sarfraz-01010 <sarfraz01010@gmail.com>
2023-07-12 23:56:01 +0500

    Result adjustments

--- app/src/main/java/com/example/quizapp/QuizResult.java
+++ app/src/main/java/com/example/quizapp/QuizResult.java
@@ -22,3 +22,35 @@ public class QuizResult extends Fragment {
     public QuizResult() {
         // Required empty public constructor
     }
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_quiz_result, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        RecyclerView recyclerView = view.findViewById(R.id.recycler_view);
+        recyclerView.setLayoutManager(new LinearLayoutManager(requireContext()));
+
+        dbHandler = new DbHandler(requireContext());
+
+        List<Questions> quizResults = dbHandler.getAnswers();
+        dbHandler.removeAllEntriesFromTable();
+        if (quizResults.isEmpty()) {
+            Toast.makeText(requireContext(), "No quiz results available.", Toast.LENGTH_SHORT).show();
+        } else {
+            RecyclerViewAdaptar quizResultAdapter = new RecyclerViewAdaptar(requireContext(), quizResults);
+            recyclerView.setAdapter(quizResultAdapter);
+        }
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        dbHandler.close();
+    }
+}
